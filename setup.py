4ea69c05c3bcbf32d3a9e505fadff418924857db
manas <manas@manashardas.com>
2016-05-28 10:32:52 -0700

    gitignore

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+setup(
+    name="sidehack",
+    version="0.0.1",
+    author="Random Walk Labs",
+    author_email="sidehack.co@gmail.com",
+    description="Enterprise innovation intelligence",
+    license="Proprietary",
+    url="https://github.com/sidehack/sidehack.git",
+    scripts = [],
+    packages=[
+        "sidehack",
+        ],
+    install_requires=[
+        "pyramda",
+        "django"
+    ],
+    package_data={},
+    tests_require=["nose"],
+    zip_safe=False,
+)
